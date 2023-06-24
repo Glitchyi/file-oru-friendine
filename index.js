9f18f1410746c6660d74369f3d9a78e0bbb5f5e6
Glitchyi <advaithnarayanan8@gmail.com>
2023-06-24 20:35:34 +0530

    :)

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ parentContainer.addEventListener("dragenter", (event) => {
 parentContainer.addEventListener("drop", (event) => {
   event.preventDefault();
   const targetContainer = event.target.closest(".container");
-  if (
-    targetContainer &&
-    targetContainer.classList.contains("disabled") == false
-  ) {
+  if (targetContainer.classList.contains("disabled") == false) {
     handleDrop(targetContainer, event.dataTransfer.files);
   }
 });
@@ -33,11 +30,11 @@ function handleDrop(container, files) {
   for (let i = 0; i < files.length; i++) {
     const file = files[i];
     if (file) {
-      if (file.type==""){
+      if (file.type == "") {
         alert("File type not supported");
-        console.log(file.type)
+        console.log(file.type);
         container.querySelector(".box").classList.remove("boxhov");
-        break;
+        return;
       }
       imageElement = container.querySelector("img");
       imageElement.style.opacity = 0;
@@ -46,11 +43,8 @@ function handleDrop(container, files) {
     }
     console.log("Container:", container);
     container.classList.add("disabled");
-    text = document.createTextNode(file.name);
-    filename = document.createElement("span");
-    filename.classList.add("text");
-    filename.appendChild(text);
-    container.appendChild(filename);
+    container.querySelector(".text").innerHTML = file.name;
+    container.querySelector(".text").style.opacity = 1;
     console.log("File:", file.name);
     console.log("Size:", file.size);
     console.log("Type:", file.type);
